acc4a1bc7a66fb20ed7c16bb10d9b5ba47163ce7
Jin Wang <specter01wj@gmail.com>
2026-01-19 21:26:07 -0500

    v3

--- Cracking the Coding Interview/mysol_ed6/Chapter6_BigO/chap6_exp2/src/main/java/com/jinwang/chap6_exp2/Chap6_exp2.java
+++ Cracking the Coding Interview/mysol_ed6/Chapter6_BigO/chap6_exp2/src/main/java/com/jinwang/chap6_exp2/Chap6_exp2.java
@@ -1,16 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.jinwang.chap6_exp2;
 
-/**
- *
- * @author jinwang
- */
+
 public class Chap6_exp2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Chap6_exp2 testCase1 = new Chap6_exp2();
+
+        int[] array = {1, 2, 3};
+        testCase1.printPairs(array);
+    }
+    
+    void printPairs(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                System.out.println(array[i] + ", " + array[j]);
+            }
+        }
     }
 }
