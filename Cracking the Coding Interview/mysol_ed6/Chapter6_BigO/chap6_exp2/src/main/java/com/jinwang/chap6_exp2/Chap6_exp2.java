c68b5f478cb8f67cf6bc17ae09260156e386d832
Jin Wang <specter01wj@gmail.com>
2026-01-19 20:55:21 -0500

    v1

---
+++ Cracking the Coding Interview/mysol_ed6/Chapter6_BigO/chap6_exp2/src/main/java/com/jinwang/chap6_exp2/Chap6_exp2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jinwang.chap6_exp2;
+
+/**
+ *
+ * @author jinwang
+ */
+public class Chap6_exp2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
