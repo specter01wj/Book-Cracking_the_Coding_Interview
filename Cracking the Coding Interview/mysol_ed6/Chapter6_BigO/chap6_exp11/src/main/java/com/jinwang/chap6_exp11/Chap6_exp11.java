a03f633d4f111ca24a00fdc2e6a40e3adc9ae441
Jin Wang <specter01wj@gmail.com>
2026-01-22 11:43:51 -0500

    v2

--- Cracking the Coding Interview/mysol_ed6/Chapter6_BigO/chap6_exp11/src/main/java/com/jinwang/chap6_exp11/Chap6_exp11.java
+++ Cracking the Coding Interview/mysol_ed6/Chapter6_BigO/chap6_exp11/src/main/java/com/jinwang/chap6_exp11/Chap6_exp11.java
@@ -1,16 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.jinwang.chap6_exp11;
 
-/**
- *
- * @author jinwang
- */
+
 public class Chap6_exp11 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Chap6_exp11 testCase1 = new Chap6_exp11();
+
+        int[] testValues = {0, 1, 3, 5, 7};
+
+        for (int n : testValues) {
+            System.out.println(n + "! = " + testCase1.factorial(n));
+        }
+    }
+    
+    int factorial(int n) {
+        if (n < 0) {
+            return -1; // error case
+        } else if (n == 0) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
     }
 }
