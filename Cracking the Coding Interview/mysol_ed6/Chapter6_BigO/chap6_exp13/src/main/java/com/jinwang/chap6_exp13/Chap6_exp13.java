2c422a3cdbd92ee5713dc339268d237e4fde856e
Jin Wang <specter01wj@gmail.com>
2026-01-22 12:03:49 -0500

    v3

--- Cracking the Coding Interview/mysol_ed6/Chapter6_BigO/chap6_exp13/src/main/java/com/jinwang/chap6_exp13/Chap6_exp13.java
+++ Cracking the Coding Interview/mysol_ed6/Chapter6_BigO/chap6_exp13/src/main/java/com/jinwang/chap6_exp13/Chap6_exp13.java
@@ -5,6 +5,21 @@ package com.jinwang.chap6_exp13;
 public class Chap6_exp13 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Chap6_exp13 testCase1 = new Chap6_exp13();
+
+        int[] testValues = {0, 1, 2, 3, 5, 10};
+
+        for (int n : testValues) {
+            System.out.println("fib(" + n + ") = " + testCase1.fib(n));
+        }
+    }
+    
+    int fib(int n) {
+        if (n <= 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        }
+        return fib(n - 1) + fib(n - 2);
     }
 }
