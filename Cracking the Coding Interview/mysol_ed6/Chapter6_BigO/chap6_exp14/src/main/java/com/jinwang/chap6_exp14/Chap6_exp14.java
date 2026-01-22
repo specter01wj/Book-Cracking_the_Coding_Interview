ff7cd7b0f805311ff42f8f432dbbfd083506b29d
Jin Wang <specter01wj@gmail.com>
2026-01-22 12:39:06 -0500

    v5

--- Cracking the Coding Interview/mysol_ed6/Chapter6_BigO/chap6_exp14/src/main/java/com/jinwang/chap6_exp14/Chap6_exp14.java
+++ Cracking the Coding Interview/mysol_ed6/Chapter6_BigO/chap6_exp14/src/main/java/com/jinwang/chap6_exp14/Chap6_exp14.java
@@ -5,6 +5,25 @@ package com.jinwang.chap6_exp14;
 public class Chap6_exp14 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Chap6_exp14 testCase1 = new Chap6_exp14();
+
+        int n = 10;
+        System.out.println("Fibonacci numbers from 0 to " + n + ":");
+        testCase1.allFib(n);
+    }
+    
+    void allFib(int n) {
+        for (int i = 0; i <= n; i++) {
+            System.out.println(i + ": " + fib(i));
+        }
+    }
+
+    int fib(int n) {
+        if (n <= 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        }
+        return fib(n - 1) + fib(n - 2);
     }
 }
