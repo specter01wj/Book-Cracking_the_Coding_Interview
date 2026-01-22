a290737ff695edf727cc1757e14f1aec85040a4e
Jin Wang <specter01wj@gmail.com>
2026-01-22 11:50:02 -0500

    v4

--- Cracking the Coding Interview/mysol_ed6/Chapter6_BigO/chap6_exp12/src/main/java/com/jinwang/chap6_exp12/Chap6_exp12.java
+++ Cracking the Coding Interview/mysol_ed6/Chapter6_BigO/chap6_exp12/src/main/java/com/jinwang/chap6_exp12/Chap6_exp12.java
@@ -5,6 +5,25 @@ package com.jinwang.chap6_exp12;
 public class Chap6_exp12 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Chap6_exp12 testCase1 = new Chap6_exp12();
+
+        String input = "abc";
+        System.out.println("Permutations of \"" + input + "\":");
+        testCase1.permutation(input);
+    }
+    
+    void permutation(String str) {
+        permutation(str, "");
+    }
+
+    void permutation(String str, String prefix) {
+        if (str.length() == 0) {
+            System.out.println(prefix);
+        } else {
+            for (int i = 0; i < str.length(); i++) {
+                String rem = str.substring(0, i) + str.substring(i + 1);
+                permutation(rem, prefix + str.charAt(i));
+            }
+        }
     }
 }
