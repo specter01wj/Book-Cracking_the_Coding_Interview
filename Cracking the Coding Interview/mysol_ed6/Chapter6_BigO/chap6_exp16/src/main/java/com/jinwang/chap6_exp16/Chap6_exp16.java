92b39e038cf78e1b11bd2746b97735ac06dde98c
Jin Wang <specter01wj@gmail.com>
2026-01-22 15:51:41 -0500

    v9

--- Cracking the Coding Interview/mysol_ed6/Chapter6_BigO/chap6_exp16/src/main/java/com/jinwang/chap6_exp16/Chap6_exp16.java
+++ Cracking the Coding Interview/mysol_ed6/Chapter6_BigO/chap6_exp16/src/main/java/com/jinwang/chap6_exp16/Chap6_exp16.java
@@ -5,6 +5,24 @@ package com.jinwang.chap6_exp16;
 public class Chap6_exp16 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Chap6_exp16 testCase1 = new Chap6_exp16();
+
+        int n = 50;
+        System.out.println("Powers of 2 up to " + n + ":");
+        testCase1.powersOf2(n);
+    }
+    
+    int powersOf2(int n) {
+        if (n < 1) {
+            return 0;
+        } else if (n == 1) {
+            System.out.println(1);
+            return 1;
+        } else {
+            int prev = powersOf2(n / 2);
+            int curr = prev * 2;
+            System.out.println(curr);
+            return curr;
+        }
     }
 }
