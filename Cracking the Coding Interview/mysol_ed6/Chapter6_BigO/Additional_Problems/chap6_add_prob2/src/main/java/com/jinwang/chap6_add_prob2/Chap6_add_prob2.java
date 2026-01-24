936ea5bd387d5eabbe23908f232560e7c143be9b
Jin Wang <specter01wj@gmail.com>
2026-01-24 00:30:58 -0500

    v3

--- Cracking the Coding Interview/mysol_ed6/Chapter6_BigO/Additional_Problems/chap6_add_prob2/src/main/java/com/jinwang/chap6_add_prob2/Chap6_add_prob2.java
+++ Cracking the Coding Interview/mysol_ed6/Chapter6_BigO/Additional_Problems/chap6_add_prob2/src/main/java/com/jinwang/chap6_add_prob2/Chap6_add_prob2.java
@@ -1,16 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.jinwang.chap6_add_prob2;
 
-/**
- *
- * @author jinwang
- */
+
 public class Chap6_add_prob2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Chap6_add_prob2 testCase1 = new Chap6_add_prob2();
+
+        int a = 2;
+        int b = 5;
+
+        int result = testCase1.power(a, b);
+        System.out.println(a + " ^ " + b + " = " + result);
+    }
+    
+    int power(int a, int b) {
+        if (b < 0) {
+            return 0; // error case
+        } else if (b == 0) {
+            return 1;
+        } else {
+            return a * power(a, b - 1);
+        }
     }
 }
