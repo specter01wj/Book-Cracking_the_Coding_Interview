9f319f2c7d4df1d4fdf40c0016e7587b3dfec3dd
Jin Wang <specter01wj@gmail.com>
2026-01-24 01:13:03 -0500

    v5

---
+++ Cracking the Coding Interview/mysol_ed6/Chapter6_BigO/Additional_Problems/chap6_add_prob3/src/main/java/com/jinwang/chap6_add_prob3/Chap6_add_prob3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jinwang.chap6_add_prob3;
+
+/**
+ *
+ * @author jinwang
+ */
+public class Chap6_add_prob3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
