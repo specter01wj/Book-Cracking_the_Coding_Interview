0e9be7380a16e078819b9abee5848d7e64010ba7
Jin Wang <specter01wj@gmail.com>
2026-01-24 21:24:00 -0500

    v6

--- Cracking the Coding Interview/mysol_ed6/Chapter6_BigO/Additional_Problems/chap6_add_prob11/src/main/java/com/jinwang/chap6_add_prob11/Chap6_add_prob11.java
+++ Cracking the Coding Interview/mysol_ed6/Chapter6_BigO/Additional_Problems/chap6_add_prob11/src/main/java/com/jinwang/chap6_add_prob11/Chap6_add_prob11.java
@@ -1,16 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.jinwang.chap6_add_prob11;
 
-/**
- *
- * @author jinwang
- */
+
 public class Chap6_add_prob11 {
 
+    int numChars = 26;
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Chap6_add_prob11 testCase1 = new Chap6_add_prob11();
+
+        int k = 2;
+        System.out.println("Sorted strings of length " + k + ":");
+        testCase1.printSortedStrings(k);
+    }
+    
+    void printSortedStrings(int remaining) {
+        printSortedStrings(remaining, "");
+    }
+
+    void printSortedStrings(int remaining, String prefix) {
+        if (remaining == 0) {
+            if (isInOrder(prefix)) {
+                System.out.println(prefix);
+            }
+        } else {
+            for (int i = 0; i < numChars; i++) {
+                char c = ithLetter(i);
+                printSortedStrings(remaining - 1, prefix + c);
+            }
+        }
+    }
+
+    boolean isInOrder(String s) {
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i - 1) > s.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    char ithLetter(int i) {
+        return (char) ('a' + i);
     }
 }
