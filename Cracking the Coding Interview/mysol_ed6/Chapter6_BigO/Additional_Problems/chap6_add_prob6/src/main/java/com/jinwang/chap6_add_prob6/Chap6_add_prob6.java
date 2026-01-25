a8032b0d4350c47850efdf57d27a2670fd68af6c
Jin Wang <specter01wj@gmail.com>
2026-01-24 20:58:56 -0500

    v1

--- Cracking the Coding Interview/mysol_ed6/Chapter6_BigO/Additional_Problems/chap6_add_prob6/src/main/java/com/jinwang/chap6_add_prob6/Chap6_add_prob6.java
+++ Cracking the Coding Interview/mysol_ed6/Chapter6_BigO/Additional_Problems/chap6_add_prob6/src/main/java/com/jinwang/chap6_add_prob6/Chap6_add_prob6.java
@@ -5,6 +5,21 @@ package com.jinwang.chap6_add_prob6;
 public class Chap6_add_prob6 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Chap6_add_prob6 testCase1 = new Chap6_add_prob6();
+
+        int n1 = 100;
+        int n2 = 50;
+
+        System.out.println("sqrt(" + n1 + ") = " + testCase1.sqrt(n1));
+        System.out.println("sqrt(" + n2 + ") = " + testCase1.sqrt(n2));
+    }
+    
+    int sqrt(int n) {
+        for (int guess = 1; guess * guess <= n; guess++) {
+            if (guess * guess == n) {
+                return guess;
+            }
+        }
+        return -1;
     }
 }
