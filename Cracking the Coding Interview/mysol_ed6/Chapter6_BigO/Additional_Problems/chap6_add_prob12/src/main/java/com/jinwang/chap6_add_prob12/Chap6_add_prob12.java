7952a4d5658bfc13c92dfcd8763c59947753de01
Jin Wang <specter01wj@gmail.com>
2026-01-24 21:40:24 -0500

    v9

--- Cracking the Coding Interview/mysol_ed6/Chapter6_BigO/Additional_Problems/chap6_add_prob12/src/main/java/com/jinwang/chap6_add_prob12/Chap6_add_prob12.java
+++ Cracking the Coding Interview/mysol_ed6/Chapter6_BigO/Additional_Problems/chap6_add_prob12/src/main/java/com/jinwang/chap6_add_prob12/Chap6_add_prob12.java
@@ -1,10 +1,29 @@
 
 package com.jinwang.chap6_add_prob12;
 
+import java.util.Arrays;
 
 public class Chap6_add_prob12 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Chap6_add_prob12 testCase1 = new Chap6_add_prob12();
+
+        int[] a = {1, 3, 5, 7, 9};
+        int[] b = {2, 3, 5, 8, 10};
+
+        int result = testCase1.intersection(a, b);
+        System.out.println("Intersection count = " + result);
+    }
+    
+    int intersection(int[] a, int[] b) {
+        Arrays.sort(b); // mergesort / quicksort under the hood
+
+        int intersect = 0;
+        for (int x : a) {
+            if (Arrays.binarySearch(b, x) >= 0) {
+                intersect++;
+            }
+        }
+        return intersect;
     }
 }
