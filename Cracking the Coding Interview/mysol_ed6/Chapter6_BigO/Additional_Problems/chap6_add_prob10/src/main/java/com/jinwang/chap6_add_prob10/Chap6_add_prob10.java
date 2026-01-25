6633015e90a832ebee4b3b102da987b003a72552
Jin Wang <specter01wj@gmail.com>
2026-01-24 21:15:42 -0500

    v4

--- Cracking the Coding Interview/mysol_ed6/Chapter6_BigO/Additional_Problems/chap6_add_prob10/src/main/java/com/jinwang/chap6_add_prob10/Chap6_add_prob10.java
+++ Cracking the Coding Interview/mysol_ed6/Chapter6_BigO/Additional_Problems/chap6_add_prob10/src/main/java/com/jinwang/chap6_add_prob10/Chap6_add_prob10.java
@@ -5,6 +5,19 @@ package com.jinwang.chap6_add_prob10;
 public class Chap6_add_prob10 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Chap6_add_prob10 testCase1 = new Chap6_add_prob10();
+
+        int n = 12345;
+        int result = testCase1.sumDigits(n);
+        System.out.println("Sum of digits of " + n + " = " + result);
+    }
+    
+    int sumDigits(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
     }
 }
