4d3d7208dc2c8850dd2380308ce640698030bd72
Jin Wang <specter01wj@gmail.com>
2026-01-25 14:29:31 -0500

    v2

--- Cracking the Coding Interview/mysol_ed6/Chapter9_InterviewQuestions/Chapter1_ArraysAndStrings/chap1_isUnique/src/main/java/com/jinwang/chap1_isunique/Chap1_isUnique.java
+++ Cracking the Coding Interview/mysol_ed6/Chapter9_InterviewQuestions/Chapter1_ArraysAndStrings/chap1_isUnique/src/main/java/com/jinwang/chap1_isunique/Chap1_isUnique.java
@@ -1,16 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.jinwang.chap1_isunique;
 
-/**
- *
- * @author jinwang
- */
+
 public class Chap1_isUnique {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Chap1_isUnique testCase1 = new Chap1_isUnique();
+        
+        System.out.println(testCase1.isUniqueChars("abcdef"));
+        System.out.println(testCase1.isUniqueChars("hello")); 
+        System.out.println(testCase1.isUniqueChars("")); 
+        
+        System.out.println(testCase1.isUniqueNoDS("abcdef")); 
+        System.out.println(testCase1.isUniqueNoDS("apple")); 
     }
+    
+    // Follow-up: no additional data structures
+    public boolean isUniqueNoDS(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j < str.length(); j++) {
+                if (str.charAt(i) == str.charAt(j)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    public boolean isUniqueChars(String str) {
+        if (str.length() > 128) return false;
+        
+        boolean[] seen = new boolean[128];
+        
+        for (int i = 0; i < str.length(); i++) {
+            int c = str.charAt(i);
+            if (seen[c]) {
+                return false;
+            }
+            seen[c] = true;
+        }
+        
+        return true;
+    }
+    
 }
