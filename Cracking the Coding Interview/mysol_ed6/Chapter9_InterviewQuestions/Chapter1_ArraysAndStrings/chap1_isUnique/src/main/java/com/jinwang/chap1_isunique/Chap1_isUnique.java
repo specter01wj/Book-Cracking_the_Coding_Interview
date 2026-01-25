82dd6d57274de564e90693872032bd15b461ac4b
Jin Wang <specter01wj@gmail.com>
2026-01-25 14:12:10 -0500

    v1

---
+++ Cracking the Coding Interview/mysol_ed6/Chapter9_InterviewQuestions/Chapter1_ArraysAndStrings/chap1_isUnique/src/main/java/com/jinwang/chap1_isunique/Chap1_isUnique.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jinwang.chap1_isunique;
+
+/**
+ *
+ * @author jinwang
+ */
+public class Chap1_isUnique {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
