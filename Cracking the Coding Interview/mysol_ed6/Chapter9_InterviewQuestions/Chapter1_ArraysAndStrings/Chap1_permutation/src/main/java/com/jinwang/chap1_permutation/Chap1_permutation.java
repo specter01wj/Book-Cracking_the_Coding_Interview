fa6712ec177ac11f8837ad7b3a10446bb6e8cfab
Jin Wang <specter01wj@gmail.com>
2026-01-27 16:09:48 -0500

    v1

---
+++ Cracking the Coding Interview/mysol_ed6/Chapter9_InterviewQuestions/Chapter1_ArraysAndStrings/Chap1_permutation/src/main/java/com/jinwang/chap1_permutation/Chap1_permutation.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jinwang.chap1_permutation;
+
+/**
+ *
+ * @author jinwang
+ */
+public class Chap1_permutation {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
