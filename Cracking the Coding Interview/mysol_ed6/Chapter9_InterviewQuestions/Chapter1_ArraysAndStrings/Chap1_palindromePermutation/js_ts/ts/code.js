3d6ea228a480876112918d37a29881c8641d993c
Jin Wang <specter01wj@gmail.com>
2026-01-30 00:00:00 -0500

    v6

---
+++ Cracking the Coding Interview/mysol_ed6/Chapter9_InterviewQuestions/Chapter1_ArraysAndStrings/Chap1_palindromePermutation/js_ts/ts/code.js
@@ -0,0 +1,58 @@
+function getCharNumber(c) {
+    const code = c.toLowerCase().charCodeAt(0);
+    if (code >= 97 && code <= 122)
+        return code - 97;
+    return -1;
+}
+// Solution 1
+function isPermutationOfPalindrome(str) {
+    const table = new Array(26).fill(0);
+    for (const c of str) {
+        const x = getCharNumber(c);
+        if (x !== -1)
+            table[x]++;
+    }
+    let foundOdd = false;
+    for (const count of table) {
+        if (count % 2 === 1) {
+            if (foundOdd)
+                return false;
+            foundOdd = true;
+        }
+    }
+    return true;
+}
+// Solution 2
+function isPermutationOfPalindromeOptimized(str) {
+    const table = new Array(26).fill(0);
+    let countOdd = 0;
+    for (const c of str) {
+        const x = getCharNumber(c);
+        if (x !== -1) {
+            table[x]++;
+            countOdd += (table[x] % 2 === 1) ? 1 : -1;
+        }
+    }
+    return countOdd <= 1;
+}
+// Solution 3
+function isPermutationOfPalindromeBitVector(str) {
+    let bitVector = 0;
+    for (const c of str) {
+        const x = getCharNumber(c);
+        if (x !== -1) {
+            bitVector ^= (1 << x);
+        }
+    }
+    return bitVector === 0 || (bitVector & (bitVector - 1)) === 0;
+}
+// Tests
+const tests = ["Tact Coa", "racecar", "hello"];
+let output = ">>> CTCI Chapter 1.4 – Palindrome Permutation <<<br><br>";
+tests.forEach(s => {
+    output += `"${s}" → `
+        + isPermutationOfPalindrome(s) + " | "
+        + isPermutationOfPalindromeOptimized(s) + " | "
+        + isPermutationOfPalindromeBitVector(s) + "<br>";
+});
+document.querySelector('#t1').innerHTML = output;
