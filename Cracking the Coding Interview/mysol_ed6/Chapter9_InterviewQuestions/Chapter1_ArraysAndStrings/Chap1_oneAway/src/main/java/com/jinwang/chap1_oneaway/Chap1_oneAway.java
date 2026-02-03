a76dd1ec71a952a1cf2ff9750cf54cbb4a91001e
Jin Wang <specter01wj@gmail.com>
2026-02-03 12:30:12 -0500

    v1

---
+++ Cracking the Coding Interview/mysol_ed6/Chapter9_InterviewQuestions/Chapter1_ArraysAndStrings/Chap1_oneAway/src/main/java/com/jinwang/chap1_oneaway/Chap1_oneAway.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jinwang.chap1_oneaway;
+
+/**
+ *
+ * @author jinwang
+ */
+public class Chap1_oneAway {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
