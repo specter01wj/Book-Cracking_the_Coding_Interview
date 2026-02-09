208b74b58abfcbce66c97e292666bd8190a58a7f
Jin Wang <specter01wj@gmail.com>
2026-02-09 14:52:08 -0500

    v6

---
+++ Cracking the Coding Interview/mysol_ed6/Chapter9_InterviewQuestions/Chapter1_ArraysAndStrings/Chap1_rotateMatrix/js_ts/ts/code.js
@@ -0,0 +1,26 @@
+function rotate(matrix) {
+    const n = matrix.length;
+    for (let layer = 0; layer < n / 2; layer++) {
+        let first = layer;
+        let last = n - 1 - layer;
+        for (let i = first; i < last; i++) {
+            let offset = i - first;
+            let top = matrix[first][i];
+            matrix[first][i] = matrix[last - offset][first];
+            matrix[last - offset][first] = matrix[last][last - offset];
+            matrix[last][last - offset] = matrix[i][last];
+            matrix[i][last] = top;
+        }
+    }
+}
+// Test
+let matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+let output = ">>> CTCI Chapter 1.7 – Rotate Matrix <<<br><br>";
+output += "Original:<br>" + JSON.stringify(matrix) + "<br><br>";
+rotate(matrix);
+output += "Rotated:<br>" + JSON.stringify(matrix);
+document.querySelector('#t1').innerHTML = output;
