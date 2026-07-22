7a6f39a57fb5944e7282835e1e60de3abb9efc5a
Jin Wang <specter01wj@gmail.com>
2026-07-22 17:39:31 -0400

    v1

---
+++ Cracking the Coding Interview/mysol_ed6/Chapter9_InterviewQuestions/Chapter2_LinkedLists/Chap2_returnKthToLast/src/main/java/com/jinwang/chap2_returnkthtolast/Chap2_returnKthToLast.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jinwang.chap2_returnkthtolast;
+
+/**
+ *
+ * @author jinwang
+ */
+public class Chap2_returnKthToLast {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
