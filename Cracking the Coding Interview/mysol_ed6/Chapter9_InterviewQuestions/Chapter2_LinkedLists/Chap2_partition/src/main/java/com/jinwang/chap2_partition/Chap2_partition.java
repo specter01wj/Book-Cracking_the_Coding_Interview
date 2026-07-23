8fafad8f972e3453fedf215ede840478b35a4741
Jin Wang <specter01wj@gmail.com>
2026-07-23 12:32:05 -0400

    v1

---
+++ Cracking the Coding Interview/mysol_ed6/Chapter9_InterviewQuestions/Chapter2_LinkedLists/Chap2_partition/src/main/java/com/jinwang/chap2_partition/Chap2_partition.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jinwang.chap2_partition;
+
+/**
+ *
+ * @author jinwang
+ */
+public class Chap2_partition {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
