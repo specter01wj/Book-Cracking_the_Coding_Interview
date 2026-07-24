2b121abf622fdec1117cd3eb99e6c84833c5e47a
Jin Wang <specter01wj@gmail.com>
2026-07-24 18:44:28 -0400

    v3

--- Cracking the Coding Interview/mysol_ed6/Chapter9_InterviewQuestions/Chapter2_LinkedLists/Chap2_sumLists/src/main/java/com/jinwang/chap2_sumlists/Chap2_sumLists.java
+++ Cracking the Coding Interview/mysol_ed6/Chapter9_InterviewQuestions/Chapter2_LinkedLists/Chap2_sumLists/src/main/java/com/jinwang/chap2_sumlists/Chap2_sumLists.java
@@ -233,4 +233,90 @@ public class Chap2_sumLists {
 
         return result;
     }
+    
+    //====================================================
+    // Solution 2 (Book Follow Up)
+    // Digits stored in forward order
+    //====================================================
+
+    public LinkedListNode addLists(LinkedListNode l1,
+                                   LinkedListNode l2) {
+
+        int len1 = length(l1);
+        int len2 = length(l2);
+
+        // Pad the shorter list with zeros.
+        if (len1 < len2) {
+            l1 = padList(l1, len2 - len1);
+        } else if (len2 < len1) {
+            l2 = padList(l2, len1 - len2);
+        }
+
+        PartialSum sum = addListsHelper(l1, l2);
+
+        if (sum.carry == 0) {
+            return sum.sum;
+        } else {
+            return insertBefore(sum.sum, sum.carry);
+        }
+    }
+
+    private PartialSum addListsHelper(LinkedListNode l1,
+                                      LinkedListNode l2) {
+
+        if (l1 == null && l2 == null) {
+            return new PartialSum();
+        }
+
+        PartialSum sum = addListsHelper(l1.next, l2.next);
+
+        int val = sum.carry + l1.data + l2.data;
+
+        LinkedListNode fullResult =
+                insertBefore(sum.sum, val % 10);
+
+        sum.sum = fullResult;
+        sum.carry = val / 10;
+
+        return sum;
+    }
+    
+    //====================================================
+    // Helpers for Solution 2
+    //====================================================
+
+    public LinkedListNode padList(LinkedListNode l, int padding) {
+
+        LinkedListNode head = l;
+
+        for (int i = 0; i < padding; i++) {
+            head = insertBefore(head, 0);
+        }
+
+        return head;
+    }
+
+    public LinkedListNode insertBefore(LinkedListNode list, int data) {
+
+        LinkedListNode node = new LinkedListNode(data);
+
+        if (list != null) {
+            node.next = list;
+        }
+
+        return node;
+    }
+
+    public int length(LinkedListNode head) {
+
+        int size = 0;
+
+        while (head != null) {
+            size++;
+            head = head.next;
+        }
+
+        return size;
+    }
+
 }
