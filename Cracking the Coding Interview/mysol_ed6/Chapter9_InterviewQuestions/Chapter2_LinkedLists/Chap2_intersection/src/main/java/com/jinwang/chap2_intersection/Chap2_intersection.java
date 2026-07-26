996492c51e79f1cfea7b08e97f8c8bbf2d06d982
Jin Wang <specter01wj@gmail.com>
2026-07-26 13:24:25 -0400

    v3

--- Cracking the Coding Interview/mysol_ed6/Chapter9_InterviewQuestions/Chapter2_LinkedLists/Chap2_intersection/src/main/java/com/jinwang/chap2_intersection/Chap2_intersection.java
+++ Cracking the Coding Interview/mysol_ed6/Chapter9_InterviewQuestions/Chapter2_LinkedLists/Chap2_intersection/src/main/java/com/jinwang/chap2_intersection/Chap2_intersection.java
@@ -107,5 +107,157 @@ public class Chap2_intersection {
         );
     }
     
-    
+    LinkedListNode findIntersection(
+            LinkedListNode list1,
+            LinkedListNode list2) {
+
+        if (list1 == null || list2 == null) {
+            return null;
+        }
+
+        Result result1 = getTailAndSize(list1);
+        Result result2 = getTailAndSize(list2);
+
+        if (result1.tail != result2.tail) {
+            return null;
+        }
+
+        LinkedListNode shorter =
+                result1.size < result2.size ? list1 : list2;
+
+        LinkedListNode longer =
+                result1.size < result2.size ? list2 : list1;
+
+        longer = getKthNode(
+                longer,
+                Math.abs(result1.size - result2.size)
+        );
+
+        while (shorter != longer) {
+            shorter = shorter.next;
+            longer = longer.next;
+        }
+
+        return longer;
+    }
+
+    Result getTailAndSize(LinkedListNode list) {
+        if (list == null) {
+            return null;
+        }
+
+        int size = 1;
+        LinkedListNode current = list;
+
+        while (current.next != null) {
+            size++;
+            current = current.next;
+        }
+
+        return new Result(current, size);
+    }
+
+    LinkedListNode getKthNode(LinkedListNode head, int k) {
+        LinkedListNode current = head;
+
+        while (k > 0 && current != null) {
+            current = current.next;
+            k--;
+        }
+
+        return current;
+    }
+
+    LinkedListNode createList(int... values) {
+        if (values.length == 0) {
+            return null;
+        }
+
+        LinkedListNode head = new LinkedListNode(values[0]);
+        LinkedListNode current = head;
+
+        for (int i = 1; i < values.length; i++) {
+            current.next = new LinkedListNode(values[i]);
+            current = current.next;
+        }
+
+        return head;
+    }
+
+    LinkedListNode getLastNode(LinkedListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        LinkedListNode current = head;
+
+        while (current.next != null) {
+            current = current.next;
+        }
+
+        return current;
+    }
+
+    void runTest(
+            String testName,
+            LinkedListNode list1,
+            LinkedListNode list2,
+            LinkedListNode expected) {
+
+        LinkedListNode actual = findIntersection(list1, list2);
+
+        System.out.println(testName);
+        System.out.println("List 1: " + listToString(list1));
+        System.out.println("List 2: " + listToString(list2));
+        System.out.println("Expected: " + nodeValue(expected));
+        System.out.println("Actual:   " + nodeValue(actual));
+        System.out.println(
+                "Result:   " + (actual == expected ? "PASS" : "FAIL")
+        );
+        System.out.println();
+    }
+
+    String listToString(LinkedListNode head) {
+        if (head == null) {
+            return "null";
+        }
+
+        StringBuilder output = new StringBuilder();
+        LinkedListNode current = head;
+
+        while (current != null) {
+            output.append(current.data);
+
+            if (current.next != null) {
+                output.append(" -> ");
+            }
+
+            current = current.next;
+        }
+
+        return output.toString();
+    }
+
+    String nodeValue(LinkedListNode node) {
+        return node == null ? "null" : String.valueOf(node.data);
+    }
+}
+
+class LinkedListNode {
+    int data;
+    LinkedListNode next;
+
+    LinkedListNode(int data) {
+        this.data = data;
+    }
+}
+
+class Result {
+    LinkedListNode tail;
+    int size;
+
+    Result(LinkedListNode tail, int size) {
+        this.tail = tail;
+        this.size = size;
+    }
 }
