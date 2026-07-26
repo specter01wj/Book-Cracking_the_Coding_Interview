112f74adfe1ac8c5680de1e80e6c6d29ae1c97a2
Jin Wang <specter01wj@gmail.com>
2026-07-26 13:17:52 -0400

    v1

---
+++ Cracking the Coding Interview/mysol_ed6/Chapter9_InterviewQuestions/Chapter2_LinkedLists/Chap2_intersection/src/main/java/com/jinwang/chap2_intersection/Chap2_intersection.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jinwang.chap2_intersection;
+
+/**
+ *
+ * @author jinwang
+ */
+public class Chap2_intersection {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
