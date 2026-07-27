fd19b9780864d746c293985a013489b3e8545ac6
Jin Wang <specter01wj@gmail.com>
2026-07-26 20:17:49 -0400

    v1

---
+++ Cracking the Coding Interview/mysol_ed6/Chapter9_InterviewQuestions/Chapter2_LinkedLists/Chap2_loopDetection/src/main/java/com/jinwang/chap2_loopdetection/Chap2_loopDetection.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jinwang.chap2_loopdetection;
+
+/**
+ *
+ * @author jinwang
+ */
+public class Chap2_loopDetection {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
