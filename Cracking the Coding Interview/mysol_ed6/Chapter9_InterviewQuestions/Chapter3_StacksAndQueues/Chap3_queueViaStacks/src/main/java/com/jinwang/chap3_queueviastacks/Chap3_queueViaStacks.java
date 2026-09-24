78ca0972972c3020cc6f55bd9c9eac39ebe69c87
Jin Wang <specter01wj@gmail.com>
2026-09-24 14:32:18 -0400

    v1

---
+++ Cracking the Coding Interview/mysol_ed6/Chapter9_InterviewQuestions/Chapter3_StacksAndQueues/Chap3_queueViaStacks/src/main/java/com/jinwang/chap3_queueviastacks/Chap3_queueViaStacks.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jinwang.chap3_queueviastacks;
+
+/**
+ *
+ * @author jinwang
+ */
+public class Chap3_queueViaStacks {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
