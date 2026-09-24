852734dd208e309376a8a4b4540c62b3e4e60de1
Jin Wang <specter01wj@gmail.com>
2026-09-24 12:08:58 -0400

    v1

---
+++ Cracking the Coding Interview/mysol_ed6/Chapter9_InterviewQuestions/Chapter3_StacksAndQueues/Chap3_stackOfPlates/src/main/java/com/jinwang/chap3_stackofplates/Chap3_stackOfPlates.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jinwang.chap3_stackofplates;
+
+/**
+ *
+ * @author jinwang
+ */
+public class Chap3_stackOfPlates {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
